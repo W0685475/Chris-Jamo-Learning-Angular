8d932a2d47a791f7bb08fdfaabd8e08ec65af977
Chris Jamo <w0685475@myscc.ca>
2024-09-11 10:35:30 -0400

    Created variables and added it to html.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Chris-Jamo-Learning-Angular';
+  protected readonly firstName = firstName;
+  protected readonly age = age;
 }
+
+let firstName = "Chris";
+let age = 27;
