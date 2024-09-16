3c181eb05bf985823d784751535b31e2baa61758
Chris Jamo <w0685475@myscc.ca>
2024-09-16 13:19:50 -0400

    Assignment 2 is finished with html/css.

---
+++ src/app/Shared/Modules/movie.ts
@@ -0,0 +1,8 @@
+export interface Movie {
+  id: number;
+  title: string;
+  director: string;
+  releaseYear: number;
+  genre: string;
+  rating?: number;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {JsonPipe, NgForOf} from "@angular/common";
+import {Movie} from "./Shared/Modules/movie";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Chris-Jamo-Learning-Angular';
+
+  // Create an array of movies using the Movie interface
+  movies: Movie[] = [
+    { id: 1, title: 'Black Panther', director: 'Ryan Coogler', releaseYear: 2018, genre: 'Action/Sci-Fi', rating: 7.3 },
+    { id: 2, title: 'No Good Deed', director: 'Sam Miller', releaseYear: 2014, genre: 'Thriller/Action', rating: 5.6 },
+    { id: 3, title: 'Dumb & Dumber', director: 'Peter Farrelly', releaseYear: 1994, genre: 'Comedy' },
+    { id: 4, title: 'Selena', director: 'Gregory Nava', releaseYear: 1997, genre: 'Drama', rating: 6.9 },
+    { id: 5, title: 'No Hard Feelings', director: 'Gene Stupnitsky', releaseYear: 2023, genre: 'Comedy', rating: 6.4 },
+    { id: 6, title: 'Dirty Dancing', director: 'Emile Ardolino', releaseYear: 1987, genre: 'Romance/Dance', rating: 7.1 }
+  ];
 
 }
