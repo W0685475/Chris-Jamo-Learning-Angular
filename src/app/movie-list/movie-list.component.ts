98131ef9a30c3d0ca2d39fc8e00f9f2b1686a254
Chris Jamo <w0685475@myscc.ca>
2024-09-25 17:40:24 -0400

    Updated Content List & Changed Colours.

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import {Movie} from "../Shared/Modules/movie";
 import {MovieListItemComponent} from "../movie-list-item/movie-list-item.component";
+import {NgClass, NgForOf, NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-movie-list',
   standalone: true,
   imports: [
-    MovieListItemComponent
+    MovieListItemComponent,
+    NgForOf,
+    NgClass,
+    NgStyle
   ],
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.css'
