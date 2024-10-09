78c77bef5150d723a8677baec0e61909f0b60cc6
Chris Jamo <w0685475@myscc.ca>
2024-10-09 12:14:18 -0400

    Provide Router got set up. Defined my list of routes as well.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import {provideRouter, Routes} from "@angular/router";
 import { AppComponent } from './app/app.component';
+import { MovieListItemComponent} from "./app/movie-list-item/movie-list-item.component";
+import {MovieListComponent} from "./app/movie-list/movie-list.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  {path: '', redirectTo: '/movies', pathMatch: 'full'},
+  { path: 'movies', component: MovieListComponent },
+  { path: 'movies/:id', component: MovieListItemComponent },
+]
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+}).then(r => console.log('Bootstrap successful'));
+
